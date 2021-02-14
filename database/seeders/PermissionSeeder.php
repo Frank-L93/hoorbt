ad029f1b9500e4e0e8ee26061004aa1f62b4523c
Frank-L93 <frank.lambregts@outlook.com>
2021-02-14 15:41:27 +0100

    fix seed init

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,15 +16,35 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
+        // create permissions
+        Permission::create(['name' => 'toevoegen ladderpartij']);
+        Permission::create(['name' => 'toevoegen toernooipartij']);
+        Permission::create(['name' => 'inzien spelergegevens']);
+        Permission::create(['name' => 'inzien unieke spelergegevens']);
+        Permission::create(['name' => 'aanpassen toernooipartij']);
+        Permission::create(['name' => 'aanpassen ladderpartij']);
+        Permission::create(['name' => 'uitslag doorgeven']);
+        Permission::create(['name' => 'bekijk tegenstander']);
+        Permission::create(['name' => 'bekijk deelnemers']);
 
+        $role1 = Role::create(['name' => 'Admin']);
+        $role1->givePermissionTo('toevoegen toernooipartij');
+        $role1->givePermissionTo('inzien spelergegevens');
+        $role1->givePermissionTo('aanpassen toernooipartij');
+        $role1->givePermissionTo('aanpassen ladderpartij');
 
-
-        $role4 = Role::findByName('Toernooispeler');
+        $role4 = Role::create(['name' => 'Toernooispeler']);
         $role4->givePermissionTo('uitslag doorgeven');
         $role4->givePermissionTo('bekijk tegenstander');
         $role4->givePermissionTo('bekijk deelnemers');
 
+        $role2 = Role::create(['name' => 'Speler']);
+        $role2->givePermissionTo('toevoegen ladderpartij');
+        $role2->givePermissionTo('aanpassen toernooipartij');
+        $role2->givePermissionTo('inzien unieke spelergegevens');
 
+        $role3 = Role::create(['name' => 'super-admin']);
     }
 }
