4ae88038731a254c91006e91a4e4242ca8e5b173
Frank-L93 <frank.lambregts@outlook.com>
2021-02-14 16:12:44 +0100

    reglement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
                                     Je speelt ieder week één match tegen een andere schaker. Deze match bestaat uit twee partijen van 7 minuten + 7 seconden. Is het 1-1, dan speel je een armageddon partij waarbij zwart voldoende heeft aan remise.<br>Verlies je de match? Dan heb je nog een herkansing, je speelt dus altijd tenminste twee matches in het toernooi.
                                     Je kunt dus de finale bereiken met 1 verliesmatch achter je naam. <br>Verlies je voor het eerst in de finale, dan gaat de finale over twee matches!
                                     Er wordt geadviseerd om online te spelen, maar je mag offline spelen wanneer je je houdt aan de richtlijnen van de overheid m.b.t. Corona.<br>
-                                    Je kunt de regels hier rustig nalezen
+                                    Je kunt de <a href="/reglement.pdf" class="underline">regels</a> hier rustig nalezen
                                 </div>
                             </div>
                         </div>
